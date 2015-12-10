6443115e4b097230ad37e0c085f58b8b91231a0c
tizot <masset.camille@gmail.com>
2015-12-10 14:09:46 +0100

    Test groups retrieval

--- sigma_core/tests/factories.py
+++ sigma_core/tests/factories.py
@@ -3,6 +3,7 @@ import factory
 from django.utils.text import slugify
 
 from sigma_core.models.user import User
+from sigma_core.models.group import Group
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -12,5 +13,14 @@ class UserFactory(factory.django.DjangoModelFactory):
     firstname = factory.Faker('first_name')
     email = factory.LazyAttribute(lambda obj: '%s.%s@school.edu' % (slugify(obj.firstname), slugify(obj.lastname)))
 
+
 class AdminUserFactory(UserFactory):
     is_staff = True
+
+
+class GroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = Group
+
+    name = factory.Sequence(lambda n: 'Group %d' % n)
+    
