d72e34de631e3f6984a1810cbd8ec2b128a196de
tizot <masset.camille@gmail.com>
2015-12-12 15:37:22 +0100

    Add UserGroupFactory for future tests

--- sigma_core/tests/factories.py
+++ sigma_core/tests/factories.py
@@ -4,6 +4,8 @@ from django.utils.text import slugify
 
 from sigma_core.models.user import User
 from sigma_core.models.group import Group
+from sigma_core.models.user_group import UserGroup
+
 
 class UserFactory(factory.django.DjangoModelFactory):
     class Meta:
@@ -23,4 +25,12 @@ class GroupFactory(factory.django.DjangoModelFactory):
         model = Group
 
     name = factory.Sequence(lambda n: 'Group %d' % n)
-    
+
+
+class UserGroupFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = UserGroup
+
+    user = factory.SubFactory(UserFactory)
+    group = factory.SubFactory(GroupFactory)
+    join_date = factory.Faker('date')
