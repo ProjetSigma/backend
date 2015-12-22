2e88de94efe65f8857225c480b3d8b8f69467591
tizot <masset.camille@gmail.com>
2015-12-22 17:43:11 +0100

    Add tests for User modification

--- sigma_core/tests/test_user.py
+++ sigma_core/tests/test_user.py
@@ -13,17 +13,18 @@ class UserTests(APITestCase):
         super(UserTests, self).setUpTestData()
 
         self.user = UserFactory()
+        self.user2 = UserFactory()
         self.admin_user = AdminUserFactory()
 
         serializer = UserSerializer(self.user)
         self.user_data = serializer.data
         self.user_url = '/user/%d/' % self.user.id
 
-        self.users_list = [self.user, self.admin_user]
+        self.users_list = [self.user, self.user2, self.admin_user]
 
         self.new_user_data = {'lastname': 'Doe', 'firstname': 'John', 'email': 'john.doe@newschool.edu', 'password': 'password'}
 
-    #### List requests
+#### List requests
     def test_get_users_list_unauthed(self):
         # Client not authenticated
         response = self.client.get('/user/')
@@ -42,7 +43,7 @@ class UserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(len(response.data), len(self.users_list))
 
-    #### Get requests
+#### Get requests
     def test_get_user_unauthed(self):
         # Client is not authenticated
         response = self.client.get(self.user_url)
@@ -59,7 +60,7 @@ class UserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data, self.user_data)
 
-    #### "Get my data" requests
+#### "Get my data" requests
     def test_get_my_data_unauthed(self):
         # Client is not authenticated
         response = self.client.get('/user/me/')
@@ -72,7 +73,7 @@ class UserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_200_OK)
         self.assertEqual(response.data['id'], self.user.id)
 
-    #### Create requests
+#### Create requests
     def test_create_user_unauthed(self):
         # Client is not authenticated
         response = self.client.post('/user/', self.new_user_data)
@@ -91,8 +92,65 @@ class UserTests(APITestCase):
         self.assertEqual(response.status_code, status.HTTP_201_CREATED)
         self.assertEqual(response.data['lastname'], self.new_user_data['lastname'])
 
-    #### Modification requests
+#### Modification requests
+    def test_edit_email_wrong_permission(self):
+        # Client wants to change another user's email
+        self.client.force_authenticate(user=self.user)
+        user_data = UserSerializer(self.user2).data
+        user_data['email'] = "pi@random.org"
+        response = self.client.put("/user/%d/" % self.user2.id, user_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
 
-    #### "Change password" requests
+    def test_edit_email_nonvalid_email(self):
+        # Client wants to change his email with a non valid value
+        self.client.force_authenticate(user=self.user)
+        user_data = self.user_data.copy()
+        user_data['email'] = "ThisIsNotAnEmail"
+        response = self.client.put("/user/%d/" % self.user.id, user_data)
+        self.assertEqual(response.status_code, status.HTTP_400_BAD_REQUEST)
 
-    #### Deletion requests
+    def test_edit_email_ok(self):
+        # Client wants to change his email and succeed in
+        self.client.force_authenticate(user=self.user)
+        user_data = self.user_data.copy()
+        user_data['email'] = "pi@random.org"
+        response = self.client.put("/user/%d/" % self.user.id, user_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['email'], user_data['email'])
+        # Guarantee that tests are independant
+        self.user.email = self.user_data['email']
+        self.user.save()
+
+    def test_edit_profile_wrong_permission(self):
+        # Client wants to change another user's phone number
+        self.client.force_authenticate(user=self.user)
+        user_data = UserSerializer(self.user2).data
+        user_data['phone'] = "0123456789"
+        response = self.client.put("/user/%d/" % self.user2.id, user_data)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_edit_profile_ok(self):
+        # Client wants to change his phone number
+        self.client.force_authenticate(user=self.user)
+        user_data = self.user_data.copy()
+        user_data['phone'] = "0123456789"
+        response = self.client.put("/user/%d/" % self.user.id, user_data)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['phone'], user_data['phone'])
+        # Guarantee that tests are independant
+        self.user.phone = self.user_data['phone']
+        self.user.save()
+
+    def test_edit_lastname_wrong_permission(self):
+        # Client wants to change his lastname
+        pass
+
+    def test_edit_lastname_ok(self):
+        # Admin wants to change an user's lastname
+        pass
+
+
+
+#### "Change password" requests
+
+#### Deletion requests
