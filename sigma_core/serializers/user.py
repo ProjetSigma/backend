2e92a8cf20cdeba1ff384c0dc9f5174729cf578e
TheBirdie <ProjetSigma>
2016-01-08 17:14:48 +0000

    Remove code duplicate. Added check to set yourself admin

--- sigma_core/serializers/user.py
+++ sigma_core/serializers/user.py
@@ -5,26 +5,26 @@ from sigma_core.models.user import User
 from sigma_core.serializers.user_group import UserGroupSerializer
 
 
+class BasicUserSerializerMeta:
+    model = User
+    exclude = ('is_staff', 'is_superuser', )
+    read_only_fields = ('last_login', 'is_active', )
+    extra_kwargs = {'password': {'write_only': True, 'required': False}}
+
 class BasicUserSerializer(serializers.ModelSerializer):
     """
     Serialize an User without relations.
     """
-    class Meta:
-        model = User
-        exclude = ('is_staff', 'is_superuser', )
-        read_only_fields = ('last_login', 'is_active', )
-        extra_kwargs = {'password': {'write_only': True, 'required': False}}
+    class Meta(BasicUserSerializerMeta):
+        pass
 
 
 class BasicUserWithPermsSerializer(BasicUserSerializer):
     """
     Serialize an User without relations and add current user's permissions on the serialized User.
     """
-    class Meta:
-        model = User
-        exclude = ('is_staff', 'is_superuser', )
-        read_only_fields = ('last_login', 'is_active', )
-        extra_kwargs = {'password': {'write_only': True, 'required': False}}
+    class Meta(BasicUserSerializerMeta):
+        pass
 
     permissions = DRYPermissionsField(read_only=True)
 
@@ -33,11 +33,8 @@ class DetailedUserSerializer(BasicUserSerializer):
     """
     Serialize full data about an User.
     """
-    class Meta:
-        model = User
-        exclude = ('is_staff', 'is_superuser', )
-        read_only_fields = ('last_login', 'is_active', )
-        extra_kwargs = {'password': {'write_only': True, 'required': False}}
+    class Meta(BasicUserSerializerMeta):
+        pass
 
     memberships = UserGroupSerializer(read_only=True, many=True)
 
@@ -46,10 +43,7 @@ class DetailedUserWithPermsSerializer(DetailedUserSerializer):
     """
     Serialize full data about an User and add current user's permissions on the serialized User.
     """
-    class Meta:
-        model = User
-        exclude = ('is_staff', 'is_superuser', )
-        read_only_fields = ('last_login', 'is_active', )
-        extra_kwargs = {'password': {'write_only': True, 'required': False}}
+    class Meta(BasicUserSerializerMeta):
+        pass
 
     permissions = DRYPermissionsField(read_only=True)
