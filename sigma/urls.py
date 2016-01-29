1a3c3e7c514672ed3deec8da69175fbf12fe77fb
TheBirdie <ProjetSigma>
2016-01-29 09:22:37 +0000

    Added /validator to list available validators

--- sigma/urls.py
+++ sigma/urls.py
@@ -22,6 +22,7 @@ from sigma_core.views.group import GroupViewSet
 from sigma_core.views.group_user import GroupUserViewSet
 from sigma_core.views.group_member import GroupMemberViewSet
 from sigma_core.views.group_field import GroupFieldViewSet
+from sigma_core.views.validator import ValidatorViewSet
 
 router = routers.DefaultRouter()
 
@@ -29,6 +30,7 @@ router.register(r'user', UserViewSet)
 router.register(r'group', GroupViewSet)
 router.register(r'group-member', GroupMemberViewSet)
 router.register(r'group-field', GroupFieldViewSet)
+router.register(r'validator', ValidatorViewSet)
 
 urlpatterns = [
     url(r'^admin/', include(admin.site.urls)),
