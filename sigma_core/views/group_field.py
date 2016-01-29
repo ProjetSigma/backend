326248a8a5ca02fd3bef84a4ca232350e0bbf357
TheBirdie <ProjetSigma>
2016-01-29 16:45:15 +0000

    Less code, more magic

--- sigma_core/views/group_field.py
+++ sigma_core/views/group_field.py
@@ -40,14 +40,3 @@ class GroupFieldViewSet(mixins.CreateModelMixin,    # Only Group admin
 
         serializer.save()
         return Response(serializer.data, status=status.HTTP_201_CREATED)
-
-    def list(self, request, *args, **kwargs):
-        queryset = self.filter_queryset(self.get_queryset())
-
-        page = self.paginate_queryset(queryset)
-        if page is not None:
-            serializer = self.get_serializer(page, many=True)
-            return self.get_paginated_response(serializer.data)
-
-        serializer = self.get_serializer(queryset, many=True)
-        return Response(serializer.data)
