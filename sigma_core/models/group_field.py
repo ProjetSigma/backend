49bb1a931c69f2b4772a3c57364ad141787162e2
TheBirdie <ProjetSigma>
2016-01-29 17:10:26 +0000

    GroupField: Remove unused DRY permission check

--- sigma_core/models/group_field.py
+++ sigma_core/models/group_field.py
@@ -19,8 +19,9 @@ class GroupField(CustomField):
     def has_read_permission(request):
         return request.user.is_authenticated()
 
-    def has_object_read_permission(self, request):
-        return request.user.is_authenticated() and request.user.is_group_member(self.group)
+    # Object-level permissions are handled on GroupFieldViewSet
+    #def has_object_read_permission(self, request):
+    #    return request.user.is_authenticated() and request.user.is_group_member(self.group)
 
     @staticmethod
     def has_write_permission(request):
