93d81a3959feae0abcde15eb379b08845ee698d9
tizot <masset.camille@gmail.com>
2016-02-06 15:50:26 +0100

    Use resetdb.sh script to reset DB

--- resetdb.sh
+++ resetdb.sh
@@ -2,4 +2,4 @@
 python3 manage.py reset_db && \
 python3 manage.py migrate && \
 python3 manage.py loaddata sigma_core/fixtures/fixtures_prod.json && \
-python3 manage.py loaddata sigma_core/fixtures/fixtures.json
+python3 manage.py loaddata fixtures.json
