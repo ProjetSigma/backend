2247f330196b04ddfed060de6dbf4e87006af11d
TheBirdie <ProjetSigma>
2016-02-15 14:45:51 +0000

    Publication: first tests

---
+++ sigma_publications/tests.py
@@ -0,0 +1,104 @@
+import factory
+from faker import Factory as FakerFactory
+
+from django.utils.text import slugify
+
+from sigma_core.models.user import User
+from sigma_core.models.group import Group
+from sigma_core.models.cluster import Cluster
+from sigma_core.models.group_member import GroupMember
+from sigma_core.models.group_member_value import GroupMemberValue
+from sigma_core.models.group_field import GroupField
+
+from sigma_publications.models import Publication, GroupPost, PublicationComment
+
+################################
+##     FACTORIES              ##
+################################
+
+faker = FakerFactory.create('fr_FR')
+class PublicationFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = User
+
+    title = factory.LazyAttribute(lambda obj: faker.last_name())
+    body = factory.LazyAttribute(lambda obj: faker.text())
+
+
+class GroupPostFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = GroupPost
+
+
+class PublicationCommentFactory(factory.django.DjangoModelFactory):
+    class Meta:
+        model = PublicationComment
+
+    comment = factory.LazyAttribute(lambda obj: faker.text())
+
+
+################################
+##   PUBLICATION TESTS        ##
+################################
+
+from rest_framework import status
+from rest_framework.test import APITestCase
+
+
+class PublicationTestsSituation(APITestCase):
+    @classmethod
+    def setUpTestData(self):
+        super(APITestCase, self).setUpTestData()
+        # Situation
+        # User0
+        #   -> Group0
+        #   -> Group1
+        #   -> Group2
+        # User1
+        #   -> Group1 [Group admin]
+        # User2
+        #   -> Group2
+        self.users = UserFactory.create_batch(3)
+        self.groups = GroupFactory.create_batch(3)
+        GroupMemberFactory(user=self.users[0], group=self.groups[0])
+        GroupMemberFactory(user=self.users[0], group=self.groups[1])
+        GroupMemberFactory(user=self.users[0], group=self.groups[2])
+        GroupMemberFactory(user=self.users[1], group=self.groups[1], perm_rank=Group.ADMINISTRATOR_RANK)
+        GroupMemberFactory(user=self.users[2], group=self.groups[2])
+
+        # Routes
+        self.publications_route = '/publication/'
+
+
+class PublicationTests(PublicationTestsSituation):
+    def gen_new_publication(self, poster_user, poster_group):
+        pub = {'text' : 'Random text'}
+        if poster_user is not None:
+            pub['poster_user'] = poster_user.id
+        if poster_group is not None:
+            pub['poster_group'] = poster_group.id
+        return pub
+
+    def test_create_publication_not_authed(self):
+        response = self.client.post(self.publications_route, self.gen_new_publication(None, None))
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
+
+    def test_create_publication_as_group_not_group_member(self):
+        self.client.force_authenticate(user=self.users[1])
+        response = self.client.get(self.clusters_url, self.gen_new_publication(None, self.groups[0]))
+        self.assertEqual(response.status_code, status.HTTP_404_NOT_FOUND)
+
+    def test_create_publication_as_group_no_permission(self):
+        self.client.force_authenticate(user=self.users[0])
+        response = self.client.get(self.clusters_url, self.gen_new_publication(None, self.groups[1]))
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+
+    def test_create_publication_as_group_ok(self):
+        self.client.force_authenticate(user=self.users[1])
+        response = self.client.get(self.clusters_url, self.gen_new_publication(None, self.groups[1]))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
+
+    def test_create_publication_as_user_ok(self):
+        self.client.force_authenticate(user=self.users[0])
+        response = self.client.get(self.clusters_url, self.gen_new_publication(self.users[0], None))
+        self.assertEqual(response.status_code, status.HTTP_201_CREATED)
