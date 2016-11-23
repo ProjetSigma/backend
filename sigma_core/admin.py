94e0243606eb7a5d675883c83ab60bec05b68477
Thibault Dardinier <t.dardinier@gmail.com>
2016-11-23 18:04:54 +0100

    Separation of group and acknowledgment in files

--- sigma_core/admin.py
+++ sigma_core/admin.py
@@ -3,11 +3,16 @@ from django.contrib.auth.models import Group as AuthGroup
 
 from sigma_core.models.user import User
 from sigma_core.models.cluster import Cluster
-from sigma_core.models.group import Group, GroupAcknowledgment
+from sigma_core.models.group import Group
+from sigma_core.models.group_acknowledgment import GroupAcknowledgment
 from sigma_core.models.group_member import GroupMember
 from sigma_core.models.group_field import GroupField
 from sigma_core.models.group_field_value import GroupFieldValue
 from sigma_core.models.group_invitation import GroupInvitation
+from sigma_core.models.participation import Participation
+from sigma_core.models.publication import Publication
+from sigma_core.models.event import Event
+from sigma_core.models.shared_publication import SharedPublication
 
 
 admin.site.unregister(AuthGroup)
@@ -19,3 +24,7 @@ admin.site.register(GroupMember)
 admin.site.register(GroupField)
 admin.site.register(GroupFieldValue)
 admin.site.register(GroupInvitation)
+admin.site.register(Participation)
+admin.site.register(Publication)
+admin.site.register(Event)
+admin.site.register(SharedPublication)
--- sigma_core/models/group.py
+++ sigma_core/models/group.py
@@ -1,6 +1,5 @@
 from django.db import models
 
-
 class Group(models.Model):
     #########################
     # Constants and choices #
@@ -66,18 +65,3 @@ class Group(models.Model):
 
     def has_module_perms(self, app_label): # pragma: no cover
         return True
-
-
-class GroupAcknowledgment(models.Model):
-    subgroup = models.ForeignKey(Group, related_name='group_parents')
-    parent_group = models.ForeignKey(Group, related_name='subgroups')
-    validated = models.BooleanField(default=False)
-    delegate_admin = models.BooleanField(default=True)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-
-    def __str__(self): # pragma: no cover
-        if self.validated:
-            return "Group %s acknowledged by Group %s" % (self.subgroup.__str__(), self.parent_group.__str__())
-        else:
-            return "Group %s awaiting for acknowledgment by Group %s since %s" % (self.subgroup.__str__(), self.parent_group.__str__(), self.created.strftime("%Y-%m-%d %H:%M"))
