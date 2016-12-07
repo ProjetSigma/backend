8bbd26edcc9a08fa6fb491c2e69d878ca87bd0e7
fandejuni <t.dardinier@gmail.com>
2016-12-07 18:12:29 +0100

    Repared resetdb.sh

--- resetdb.sh
+++ resetdb.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 python3 manage.py reset_db && \
+python3 manage.py makemigrations && \
 python3 manage.py migrate && \
 python3 manage.py loaddata sigma_core/fixtures.json && \
 python3 manage.py loaddata fixtures_oauth.json
